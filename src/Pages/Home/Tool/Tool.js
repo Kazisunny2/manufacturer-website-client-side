2b88ef786be5f59301b8b55186876e467e4b495a
Kazisunny2 <saidul.bit@gmail.com>
2022-05-23 21:08:23 +0200

    Login form  added

--- src/Pages/Home/Tool/Tool.js
+++ src/Pages/Home/Tool/Tool.js
@@ -2,23 +2,23 @@ import React from "react";
 
 const Tool = ({ tool }) => {
   return (
-    <div class="card w-96 bg-base-100 shadow-xl">
-      <figure class="px-10 pt-10">
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure className="px-10 pt-10">
         <img
           src={tool.img}
           alt=""
           style={{ height: "20rem" }}
-          class="rounded-xl"
+          className="rounded-xl"
         />
       </figure>
-      <div class="card-body text-center">
-        <h2 class="card-title">{tool.name}</h2>
+      <div className="card-body text-center">
+        <h2 className="card-title">{tool.name}</h2>
         <p className="card-text">Description: {tool.description}</p>
         <p> Minimum Order Quantity: {tool.minimum_order_quantity}</p>
         <p>Available Quantity: {tool.available_quantity}</p>
         <p>Price(Per Unit): ${tool.Price}</p>
-        <div class="card-actions">
-          <button class="btn btn-primary">Buy Now</button>
+        <div className="card-actions">
+          <button className="btn btn-primary">Buy Now</button>
         </div>
       </div>
     </div>
