3aa9c65f6a7d31468c3f907956a6e9732c991c1c
Kazisunny2 <saidul.bit@gmail.com>
2022-05-28 06:30:40 +0200

    My Profile  added

--- src/Pages/Dashboard/MyProfile.js
+++ src/Pages/Dashboard/MyProfile.js
@@ -2,8 +2,30 @@ import React from "react";
 
 const MyProfile = () => {
   return (
-    <div>
-      <h2>This is my Profile</h2>
+    <div className=" text-xl ">
+      <h2>
+        <span className="text-bold text-2xl text-pink-800">Name:</span> Kazi
+        Shaidul Islam Sunny
+      </h2>
+      <h2>
+        <span className="text-pink-800 text-bold text-2xl">
+          Email address:{" "}
+        </span>
+        saidul.bit@gmail.com
+      </h2>
+      <h2>
+        <span className="text-bold text-2xl text-pink-800">
+          Educational background:{" "}
+        </span>
+        BSc in EEE and Msc in Communication Systems and Networks (Running)
+      </h2>
+      <h2>
+        <span className="text-bold text-2xl text-pink-800">
+          Technological Skills:
+        </span>
+        HTML, CSS, Bootstrap, Tailwind JavaScript, React, Nodejs, Express,
+        Mongodb
+      </h2>
     </div>
   );
 };
